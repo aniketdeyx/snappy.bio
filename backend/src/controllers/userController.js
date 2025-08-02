5dd0324a5a1be760db71a2ff8798ac4155634073
aniketdeyx <aniketdey251@gmail.com>
2025-08-02 20:05:22 +0530

    Token issue

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -30,11 +30,9 @@ export const getProfile = async (req, res) => {
   const userId = req.user.userId;
   try {
     const user = await User.findById(userId).select("-password"); // Exclude password
-
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-
     res.json({ user });
   } catch (err) {
     console.error("Error fetching profile:", err);
--- backend/src/middlewares/requireAuth.js
+++ backend/src/middlewares/requireAuth.js
@@ -2,9 +2,8 @@ import jwt from "jsonwebtoken";
 
 export const requireAuth = (req, res, next) => {
   const token = req.cookies.token;
-
-  if (!token) return res.status(401).json({ error: "Unauthorized" });
-
+  console.log("Token:", token);
+  if (!token) return res.status(401).json({ error: "Token nhi hai" });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
